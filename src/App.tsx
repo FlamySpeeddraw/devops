278466393bfc4c57084a1c2582d1d96afb2dc5b8
Sacha <sachaobiedzy@gmail.com>
2026-03-09 22:02:36 +0100

    Feat: les tests à venir

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <p>SUPER IMPORTANT A AFFICHER !!!!!!</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Les tests sont à venir !
       </p>
     </>
   )
