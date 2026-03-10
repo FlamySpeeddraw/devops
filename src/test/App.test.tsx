c72b81630a71430be8012467789231a682b82efd
Sacha <sachaobiedzy@gmail.com>
2026-03-10 18:23:34 +0100

    Feat: test du compteur

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,22 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("Le compteur démarre à 0", () => {
     render(<App />);
     expect(screen.getByText(/count is 0/i)).toBeInTheDocument();
+});
+
+test("Le compteur augmente au clic", async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    const button = screen.getByRole("button");
+    await user.click(button);
+    expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
+
+    for (const _ of Array(8)) {
+        await user.click(button);
+    }
+    expect(screen.getByText(/count is 9/i)).toBeInTheDocument();
 });
